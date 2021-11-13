14f15cb390667d19576aba8f03253d494653ae63
Katarzyna-Zamorska <katarzyna.zamorska01@gmail.com>
2021-11-13 14:57:06 +0100

    fifth change

---
+++ src/main/java/com/sda/weather/Location.java
@@ -0,0 +1,13 @@
+package com.sda.weather;
+
+import java.util.Scanner;
+
+public class Location {
+
+    String cityName;
+    String longitude;
+    String latitude;
+    String region;
+    String country;
+
+}
